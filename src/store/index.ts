ea40be0f81a52084ca7900bf1d31b17f3705b51c
pizhihui@163.com <pizhihui@163.com>
2024-09-05 23:52:34 +0800

    fix: add slices for store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { create } from 'zustand'
+import { create, StateCreator } from 'zustand'
 import { User } from '@/services/types/api'
 import { devtools } from 'zustand/middleware'
 
@@ -29,4 +29,65 @@ export const useStore = create<UserType>()(devtools((set) => ({
   name: 'user_store', // 浏览器调试时显示的 store 名称
   enabled: true, // 是否开启调试工具(通常根据当前环境开启/关闭)
 }))
+
+interface BearSlice {
+  bears: number
+  addBear: () => void
+  eatFish: () => void
+}
+
+interface FishSlice {
+  fishes: number
+  addFish: () => void
+}
+
+interface SharedSlice {
+  addBoth: () => void
+  getBoth: () => void
+}
+
+
+const createBearSlice: StateCreator<
+  BearSlice & FishSlice,
+  [],
+  [],
+  BearSlice
+> = (set) => ({
+  bears: 0,
+  addBear: () => set((state) => ({ bears: state.bears + 1 })),
+  eatFish: () => set((state) => ({ fishes: state.fishes - 1 })),
+})
+
+const createFishSlice: StateCreator<
+  BearSlice & FishSlice,
+  [],
+  [],
+  FishSlice
+> = (set) => ({
+  fishes: 0,
+  addFish: () => set((state) => ({ fishes: state.fishes + 1 })),
+})
+
+const createSharedSlice: StateCreator<
+  BearSlice & FishSlice,
+  [],
+  [],
+  SharedSlice
+> = (set, get) => ({
+  addBoth: () => {
+    // you can reuse previous methods
+    get().addBear()
+    get().addFish()
+    // or do them from scratch
+    // set((state) => ({ bears: state.bears + 1, fishes: state.fishes + 1 })
+  },
+  getBoth: () => get().bears + get().fishes,
+})
+
+const useBoundStore = create<BearSlice & FishSlice & SharedSlice>()((...a) => ({
+  ...createBearSlice(...a),
+  ...createFishSlice(...a),
+  ...createSharedSlice(...a),
+}))
+
 
